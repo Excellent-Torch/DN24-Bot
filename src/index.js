364e0afd71fd0664cc534a3f252adaeec43a7b1d
Excellent_Torch <ishanmaduranga42@gmail.com>
2024-05-30 20:28:01 +0100

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ client.on('ready', (c) => {
     console.log(`${c.user.tag} is online.`);
 
     client.user.setActivity({
-        name: 'DN24',
+        name: 'DN24 BRRRRRRRRRRRRRR',
     });
 });
 
